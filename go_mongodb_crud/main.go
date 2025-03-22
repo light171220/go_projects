578f0ef345b17e3c0b6de4b3722ad06efcc1295f
light171220 <shubgupta589@gmail.com>
2025-03-22 14:13:39 +0530

    proj: completed mongodb basic crud app

--- go_mongodb_crud/main.go
+++ go_mongodb_crud/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"go_mongodb_crud/controllers"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+func main(){
+
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:9000", r)
+}
+
+
+func getSession() *mgo.Session{
+
+	s, err := mgo.Dial("mongodb://localhost:27017")
+	if err != nil{
+		panic(err)
+	}
+	return s
+}
